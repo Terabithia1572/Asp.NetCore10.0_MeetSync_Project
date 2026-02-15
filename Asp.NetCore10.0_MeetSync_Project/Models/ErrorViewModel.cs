d630454e5dd0ecb32c8c368fc014fb7c7560904d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2026-02-15 15:04:22 +0300

    Add project files.

---
+++ Asp.NetCore10.0_MeetSync_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetCore10._0_MeetSync_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
