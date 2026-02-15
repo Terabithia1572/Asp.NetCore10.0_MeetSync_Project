c16f8ca446b86e770fabcc34c33643be0637ac4d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2026-02-15 20:25:52 +0300

    #16 feat: add meeting screen layout
    - Created MeetingController
    - Added meeting UI with video grid
    - Added chat and participants panel

---
+++ Asp.NetCore10.0_MeetSync_Project/Controllers/MeetingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_MeetSync_Project.Controllers
+{
+    public class MeetingController : Controller
+    {
+        public IActionResult Index(string roomName)
+        {
+            ViewBag.RoomName = roomName;
+            return View();
+        }
+    }
+}
